68a5612fa30a07055894e6aa9e9ef81706ba86b7
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-04-21 16:18:39 -0600

    more formatting corrections

--- src/IterativeSorterAnalysis.java
+++ src/IterativeSorterAnalysis.java
@@ -24,7 +24,7 @@ public class IterativeSorterAnalysis {
 	
 	private void analyseRandom() {
 			
-		System.out.print(String.format("%6s", "n"));
+		System.out.print(String.format("%12s\t", "n"));
 		for (int i = 0; i < 6; i++) {
 			System.out.print(String.format("%12s\t","random"));			
 		}
@@ -41,7 +41,7 @@ public class IterativeSorterAnalysis {
 		System.out.println();
 		
 		
-		System.out.print(String.format("%6s\t", "n"));
+		System.out.print(String.format("%12s\t", "n"));
 		for (int i = 0; i < 4; i++) {
 			System.out.print(String.format("%12s\t%12s\t", "BS-reads", "BS-writes"));
 			System.out.print(String.format("%12s\t%12s\t", "IS-reads", "IS-writes"));
@@ -52,70 +52,70 @@ public class IterativeSorterAnalysis {
 		for (int n = 100; n <= 10000; n+=100){
 			///////////////////////////////////RANDOM
 			IntegerArray arrayToSort = null;
-			System.out.print(String.format("%6d\t",n));
+			System.out.print(String.format("%12d\t",n));
 			
 			arrayToSort = utility.createRandomArray(n);
 			IntegerArray.reset();
 			sorter.doBubbleSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createRandomArray(n);
 			IntegerArray.reset();
 			sorter.doInsertionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createRandomArray(n);
 			IntegerArray.reset();
 			sorter.doSelectionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			///////////////////////////////////NEARLY SORTED
 			arrayToSort = utility.createNearlySortedArray(n);			
 			IntegerArray.reset();
 			sorter.doBubbleSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createNearlySortedArray(n);			
 			IntegerArray.reset();
 			sorter.doInsertionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createNearlySortedArray(n);			
 			IntegerArray.reset();
 			sorter.doSelectionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 			
 			///////////////////////////////////REVERSE ORDER
 			arrayToSort = utility.createReverseOrderArray(n);						
 			IntegerArray.reset();
 			sorter.doBubbleSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createReverseOrderArray(n);						
 			IntegerArray.reset();
 			sorter.doInsertionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createReverseOrderArray(n);						
 			IntegerArray.reset();
 			sorter.doSelectionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			///////////////////////////////////NEARLY SORTED
 			arrayToSort = utility.createFewUniqueArray(n);			
 			IntegerArray.reset();
 			sorter.doBubbleSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createFewUniqueArray(n);			
 			IntegerArray.reset();
 			sorter.doInsertionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			arrayToSort = utility.createFewUniqueArray(n);			
 			IntegerArray.reset();
 			sorter.doSelectionSort(arrayToSort);
-			System.out.print(String.format("%10d\t%10d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
+			System.out.print(String.format("%12d\t%12d\t", arrayToSort.getReads(), arrayToSort.getWrites()));
 
 			
 			System.out.println();
@@ -123,5 +123,5 @@ public class IterativeSorterAnalysis {
 		}
 		
 	}
-		
+			
 }
